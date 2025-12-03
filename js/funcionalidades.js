9e9e8a9f0861544d7958789f2f1581081161c563
Rubén Ojeda <ojedafj@alumnos.ilerna.com>
2025-12-03 16:06:20 +0100

    Cambio fondo JS funcional

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -1,5 +1,29 @@
 const btnFondo = document.getElementById('btnFondo');
 btnFondo.addEventListener('click', () => {
-    document.body.classList.remove('fondo2');
-    document.body.classList.add('fondo3');
+    cambiarFondo('fondo1');
 });
+
+function cambiarFondo(fondoNuevo) {
+    const clasesBody = document.body.classList;
+    const clasesArray = Array.from(clasesBody);
+    const fondo = clasesArray[1];
+    document.body.classList.remove(fondo);
+    document.body.classList.add(fondoNuevo);
+}
+
+
+const fondo2 = document.getElementById('fondo2');
+fondo2.addEventListener('click', () => {
+    cambiarFondo('fondo2');
+});
+
+const fondo3 = document.getElementById('fondo3');
+fondo3.addEventListener('click', () => {
+    cambiarFondo('fondo3');
+});
+
+const fondo4 = document.getElementById('fondo4');
+fondo4.addEventListener('click', () => {
+    cambiarFondo('fondo4');
+});
+
