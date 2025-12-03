db6629ea3976502ed78a4845c378def9d8500d4d
0jedv <ruben.2004.esp@gmail.com>
2025-12-03 18:08:35 +0100

    feat: Add initial website structure, styling, ScienceGothic font family, and favicon. Also implementation of the color-change on the navbar at funcionalidades.js

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -1,16 +1,22 @@
+// Boton para cambiar el fondo al fondo1.
+
 const btnFondo = document.getElementById('btnFondo');
 btnFondo.addEventListener('click', () => {
     cambiarFondo('fondo1');
 });
 
-function cambiarFondo(fondoNuevo) {
-    const clasesBody = document.body.classList;
-    const clasesArray = Array.from(clasesBody);
-    const fondo = clasesArray[1];
-    document.body.classList.remove(fondo);
-    document.body.classList.add(fondoNuevo);
-}
+// Funcion para cambiar el fondo a uno nuevo
 
+function cambiarFondo(fondoNuevo) {
+    const heroSection = document.querySelector('.hero-section');
+    const clasesHero = heroSection.classList;
+    const clasesArray = Array.from(clasesHero);
+    // Buscar la clase de fondo actual (fondo1, fondo2, fondo3, fondo4)
+    const fondoActual = clasesArray.find(clase => clase.startsWith('fondo'));
+
+    heroSection.classList.remove(fondoActual);
+    heroSection.classList.add(fondoNuevo);
+}
 
 const fondo2 = document.getElementById('fondo2');
 fondo2.addEventListener('click', () => {
@@ -27,3 +33,22 @@ fondo4.addEventListener('click', () => {
     cambiarFondo('fondo4');
 });
 
+// Cambia el color del Navbar dependiendo del fondo del Hero-section.
+
+const navbar = document.querySelector('.navbar-nav');
+navbar.addEventListener('mouseenter', () => {
+    cambiarColorNavbar();
+})
+function cambiarColorNavbar() {
+    const heroSection = document.querySelector('.hero-section');
+    const clasesHero = heroSection.classList;
+    const clasesArray = Array.from(clasesHero);
+    const fondoActual = clasesArray.find(clase => clase.startsWith('fondo'));
+    if (fondoActual === 'fondo1' || fondoActual === 'fondo3') {
+        navbar.classList.remove('nav-color-green');
+        navbar.classList.add('nav-color-red');
+    } else {
+        navbar.classList.remove('nav-color-red');
+        navbar.classList.add('nav-color-green');
+    }
+}
